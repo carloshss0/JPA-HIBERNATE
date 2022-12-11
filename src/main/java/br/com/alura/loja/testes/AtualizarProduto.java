9616b5748bb58ed74789db4555e6eb5af0384544
Carlos <carloshss97@gmail.com>
2022-12-11 09:46:11 -0300

    Primeiro commit de projeto de aplicação utilizando JPA Hibernate

---
+++ src/main/java/br/com/alura/loja/testes/AtualizarProduto.java
@@ -0,0 +1,24 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+
+public class AtualizarProduto {
+    public static void main(String[] args) {
+        Long id = 1l;
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto produto = produtoDao.buscarPorId(id);
+        em.getTransaction().begin();
+        produtoDao.atualizarProduto(produto);
+        produto.setPreco(new BigDecimal("1000"));
+        em.getTransaction().commit();
+        em.close();
+
+    }
+}
